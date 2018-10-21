b46b36edaf23d8781fea8498642c9a2c52484ee5
Thomas Helmke <thomas.helmke@gmx.de>
2018-10-21 12:58:32 +0200

    Code angepasst auf meinen Standard NTP Code.

--- esp8266-clock/esp8266-clock.ino
+++ esp8266-clock/esp8266-clock.ino
@@ -1,72 +1,65 @@
-/* -------------------------------------------------
-   NTP Clock
-   using ESP8266 and Lixie Displays!
-
-   by Connor Nishijima - 12/28/2016
-   -------------------------------------------------
-
-   To use your Lixie Displays / ESP8266 as an NTP
-   clock, you'll need a few things:
-
-   - WIFI_SSID
-   - WIFI_PASSWORD
-   - SIX_DIGIT
-   - TIME_COLOR_RGB
-   - TIME_OFFSET
-
-  SIX_DIGIT is a true/false for 6 or 4 digit clocks
-  TIME_COLOR_RGB is an 8-bit RGB value to color the displays
-  TIME_OFFSET is your local UTC offset for time zones
-
-   -------------------------------------------------
-*/
-
-#include "Lixie.h" // Include Lixie Library
-#define DATA_PIN   5
-#define NUM_LIXIES 4
+// Include Lixie Library
+#include "Lixie.h" 
+// setup number of pixels
+const bool SIX_DIGIT = false; // True if 6-digit clock with seconds
+const int NUM_LIXIES = (SIX_DIGIT) ? 6 : 4; // 4 or 6 digits
+const int DISPLAY_INTERVAL = (SIX_DIGIT) ? 1000 : 60000; // update once a second or once a minute
+// setup pin number and create Lixie object
+const int DATA_PIN = 5;
 Lixie lix(DATA_PIN, NUM_LIXIES);
 
-#include <TimeLib.h>
+//---------------------------------------
+const CRGB TIME_COLOR_RGB(0,255,255); // set default color to CYAN
+//---------------------------------------
+
+// Include time libraries
+#include <Time.h>
+#include <Timezone.h>
+// configure Central European Time (Frankfurt, Paris)
+const TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     //Central European Summer Time
+const TimeChangeRule CET = {"CET ", Last, Sun, Oct, 3, 60};       //Central European Standard Time
+Timezone CE(CEST, CET);
+
+// Include networking libraries
+#include <NTPClient.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include <WiFiUdp.h>
-#include <ESP8266WiFiMulti.h>   // WifiMulti Lib for connection handling
-ESP8266WiFiMulti WiFiMulti;
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <ArduinoOTA.h>
 
-//---------------------------------------
-const char* WIFI_SSID = "";  //  your network SSID (name)
-const char* WIFI_PASS = "";  //  your network password
+// setup NTP
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP,"de.pool.ntp.org");
 
-const bool SIX_DIGIT = false; // True if 6-digit clock with seconds
-const byte TIME_COLOR_RGB[3] = {0,255,255}; // CYAN
-const int TIME_OFFSET = -7;     // US Mountain Time
-//---------------------------------------
+// global variables
+const unsigned int updateInterval = 10000;
+unsigned int lastUpdate = 0;
 
-// NTP Servers:
-static const char ntpServerName[] = "us.pool.ntp.org";
-//static const char ntpServerName[] = "time.nist.gov";
-//static const char ntpServerName[] = "time-a.timefreq.bldrdoc.gov";
-//static const char ntpServerName[] = "time-b.timefreq.bldrdoc.gov";
-//static const char ntpServerName[] = "time-c.timefreq.bldrdoc.gov";
+bool WiFiConnected = false;
+bool NTPupdated = false;
 
-WiFiUDP Udp;
-unsigned int localPort = 8888;  // local port to listen for UDP packets
+static unsigned long msLast = 0;
 
-time_t getNtpTime();
+// declare functions
 void digitalClockDisplay();
-void sendNTPpacket(IPAddress &address);
 
 void setup()
 {
   lix.begin(); // Initialize LEDs
   Serial.begin(115200);
-  WiFiMulti.addAP(WIFI_SSID, WIFI_PASS); // Your WIFI credentials
+
+  msLast = millis();
 
   // This sets all lights to yellow while we're connecting to WIFI
-  while ((WiFiMulti.run() != WL_CONNECTED)) {
-    lix.color(255, 255, 0);
-    lix.write(8888);
-    delay(100);
-  }
+  lix.color(255, 255, 0);
+  lix.write(8888);
+
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("LixieClockAP");
+  WiFiConnected = true;
 
   // Green on connection success
   lix.color(0, 255, 0);
@@ -79,24 +72,59 @@ void setup()
 
   Serial.print("IP number assigned by DHCP is ");
   Serial.println(WiFi.localIP());
-  Serial.println("Starting UDP");
-  Udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(Udp.localPort());
-  Serial.println("waiting for sync");
-  setSyncProvider(getNtpTime);
-  setSyncInterval(300);
-}
 
-time_t prevDisplay = 0; // when the digital clock was displayed
+  // setup over the air programming
+  ArduinoOTA.setHostname("Wordclock12OTA");
+  ArduinoOTA.onStart([]() {
+          Serial.println("OTA Start");
+          });
+  ArduinoOTA.onEnd([]() {
+          Serial.println("\nOTA End");
+          });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+          Serial.printf("OTA Progress: %u%%\r", (progress / (total / 100)));
+          });
+  ArduinoOTA.onError([](ota_error_t error) {
+          Serial.printf("OTA Error[%u]: ", error);
+          if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+          else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+          else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+          else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+          else if (error == OTA_END_ERROR) Serial.println("End Failed");
+          });
+  ArduinoOTA.begin();
+
+  // start NTP Client
+  timeClient.begin();
+
+}
 
 void loop()
 {
-  if (timeStatus() != timeNotSet) {
-    if (now() != prevDisplay) { //update the display only if time has changed
-      prevDisplay = now();
+  // handle over the air programming
+  ArduinoOTA.handle();
+
+  // update display every second if SIX_DIGIT is true, else once a minute
+  unsigned long msNow = millis();
+  if((millis() - msLast >= DISPLAY_INTERVAL) || (millis() < msLast))
+  {
+      msLast = millis();
       digitalClockDisplay();
-    }
+  }
+
+  // update time from ntp
+  if((millis() - lastUpdate) > updateInterval)
+  {
+      lastUpdate = millis();
+      NTPupdated = timeClient.update();
+      if(CE.utcIsDST(timeClient.getEpochTime()))
+      {
+          timeClient.setTimeOffset(7200); // daylight saving time: UTC+2
+      }
+      else
+      {
+          timeClient.setTimeOffset(3600); // normal time: UTC+1
+      }
   }
 }
 
@@ -107,24 +135,27 @@ void digitalClockDisplay()
   String h;
   String m;
   String s;
+  int Second = timeClient.getSeconds();
+  int Minute = timeClient.getMinutes();
+  int Hour = timeClient.getHours();
 
-  if(hour() < 10){
-    h = "0"+String(hour());
+  if(Hour < 10){
+    h = "0"+String(Hour);
   }
   else{
-    h = String(hour());
+    h = String(Hour);
   }
-  if(minute() < 10){
-    m = "0"+String(minute());
-  }
-  else{
-    m = String(minute());
-  }
-  if(second() < 10){
-    s = "0"+String(second());
+  if(Minute < 10){
+    m = "0"+String(Minute);
   }
   else{
-    s = String(second());
+    m = String(Minute);
+  }
+  if(Second < 10){
+    s = "0"+String(Second);
+  }
+  else{
+    s = String(Second);
   }
 
   time_now += h;
@@ -138,66 +169,7 @@ void digitalClockDisplay()
   char buf[10];
   time_now.toCharArray(buf,10);
 
-  lix.color(TIME_COLOR_RGB[0],TIME_COLOR_RGB[1],TIME_COLOR_RGB[2]);
+  lix.color(TIME_COLOR_RGB);
   lix.write(buf);
   Serial.println(time_now);
-}
-
-/*-------- NTP code ----------*/
-
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
-
-time_t getNtpTime()
-{
-  IPAddress ntpServerIP; // NTP server's ip address
-
-  while (Udp.parsePacket() > 0) ; // discard any previously received packets
-  Serial.println("Transmit NTP Request");
-  // get a random server from the pool
-  WiFi.hostByName(ntpServerName, ntpServerIP);
-  Serial.print(ntpServerName);
-  Serial.print(": ");
-  Serial.println(ntpServerIP);
-  sendNTPpacket(ntpServerIP);
-  uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
-    int size = Udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
-      Serial.println("Receive NTP Response");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
-      unsigned long secsSince1900;
-      // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-      secsSince1900 |= (unsigned long)packetBuffer[43];
-      return secsSince1900 - 2208988800UL + TIME_OFFSET * SECS_PER_HOUR;
-    }
-  }
-  Serial.println("No NTP Response :-(");
-  return 0; // return 0 if unable to get the time
-}
-
-// send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress &address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12] = 49;
-  packetBuffer[13] = 0x4E;
-  packetBuffer[14] = 49;
-  packetBuffer[15] = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
 }
