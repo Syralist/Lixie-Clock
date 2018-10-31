645d2438cd9b0cb03468e4006737b65cc0a80275
Thomas Helmke <thomas.helmke@gmx.de>
2018-10-31 11:53:15 +0100

    set brightness and color from MQTT for use in HomeAssistant

--- esp8266-mqttjson-clock/esp8266-mqttjson-clock.ino
+++ esp8266-mqttjson-clock/esp8266-mqttjson-clock.ino
@@ -42,7 +42,7 @@ NTPClient timeClient(ntpUDP,"de.pool.ntp.org");
 
 // setup MQTT
 WiFiClient net;
-MQTTClient client;
+MQTTClient client(512);
 // define your default values here, if there are different values in config.json, they are overwritten.
 char mqtt_server[40];
 char mqtt_port[6] = "8080";
@@ -105,8 +105,11 @@ static unsigned long msLast = 0;
 void digitalClockDisplay();
 void connect();
 void messageReceived(String &topic, String &payload);
-bool processJson(String message);
+bool processJson(String &message);
+void setColor(int inR, int inG, int inB);
 void sendState();
+int calculateStep(int prevValue, int endValue);
+int calculateVal(int step, int val, int i);
 void saveConfigCallback();
 
 void setup()
@@ -312,6 +315,73 @@ void loop()
           timeClient.setTimeOffset(1*3600); // normal time: UTC+1
       }
   }
+
+  if (flash) {
+    if (startFlash) {
+      startFlash = false;
+      flashStartTime = millis();
+    }
+
+    if ((millis() - flashStartTime) <= flashLength) {
+      if ((millis() - flashStartTime) % 1000 <= 500) {
+        setColor(flashRed, flashGreen, flashBlue);
+      }
+      else {
+        setColor(0, 0, 0);
+        // If you'd prefer the flashing to happen "on top of"
+        // the current color, uncomment the next line.
+        // setColor(realRed, realGreen, realBlue);
+      }
+    }
+    else {
+      flash = false;
+      setColor(realRed, realGreen, realBlue);
+    }
+  }
+
+  if (startFade) {
+    // If we don't want to fade, skip it.
+    if (transitionTime == 0) {
+      setColor(realRed, realGreen, realBlue);
+
+      redVal = realRed;
+      grnVal = realGreen;
+      bluVal = realBlue;
+
+      startFade = false;
+    }
+    else {
+      loopCount = 0;
+      stepR = calculateStep(redVal, realRed);
+      stepG = calculateStep(grnVal, realGreen);
+      stepB = calculateStep(bluVal, realBlue);
+
+      inFade = true;
+    }
+  }
+
+  if (inFade) {
+    startFade = false;
+    unsigned long now = millis();
+    if (now - lastLoop > transitionTime) {
+      if (loopCount <= 1020) {
+        lastLoop = now;
+        
+        redVal = calculateVal(stepR, redVal, loopCount);
+        grnVal = calculateVal(stepG, grnVal, loopCount);
+        bluVal = calculateVal(stepB, bluVal, loopCount);
+        
+        setColor(redVal, grnVal, bluVal); // Write current values to LED pins
+
+        Serial.print("Loop count: ");
+        Serial.println(loopCount);
+        loopCount++;
+      }
+      else {
+        inFade = false;
+      }
+    }
+  }
 }
 
 void digitalClockDisplay()
@@ -350,7 +420,7 @@ void digitalClockDisplay()
   char buf[10];
   time_now.toCharArray(buf,10);
 
-  lix.color(TIME_COLOR_RGB);
+  // lix.color(TIME_COLOR_RGB);
   lix.write(buf);
   Serial.println(time_now);
 }
@@ -369,6 +439,9 @@ void connect() {
   }
 
   Serial.println("\nconnected!");
+  client.subscribe(mqtt_cmd_topic);
+  Serial.print("subscribed to ");
+  Serial.println(mqtt_cmd_topic);
 }
 
 void messageReceived(String &topic, String &payload) {
@@ -467,6 +540,19 @@ bool processJson(String &message) {
   return true;
 }
 
+void setColor(int inR, int inG, int inB) {
+  lix.color(inR, inG, inB);
+  lix.show();
+
+  Serial.println("Setting LEDs:");
+  Serial.print("r: ");
+  Serial.print(inR);
+  Serial.print(", g: ");
+  Serial.print(inG);
+  Serial.print(", b: ");
+  Serial.println(inB);
+}
+
 void sendState() {
   DynamicJsonBuffer jsonBuffer;
 
@@ -480,10 +566,12 @@ void sendState() {
 
   root["brightness"] = brightness;
 
-  char buffer[root.measureLength() + 1];
-  root.printTo(buffer, sizeof(buffer));
+  // char buffer[root.measureLength() + 1];
+  // root.printTo(buffer, sizeof(buffer));
+  String buffer;
+  root.printTo(buffer);
 
-  client.publish(light_state_topic, buffer, true);
+  client.publish(mqtt_topic, buffer, true, 0);
 }
 
 //callback notifying us of the need to save config
@@ -491,3 +579,68 @@ void saveConfigCallback () {
   Serial.println("Should save config");
   shouldSaveConfig = true;
 }
+
+// From https://www.arduino.cc/en/Tutorial/ColorCrossfader
+/* BELOW THIS LINE IS THE MATH -- YOU SHOULDN'T NEED TO CHANGE THIS FOR THE BASICS
+* 
+* The program works like this:
+* Imagine a crossfade that moves the red LED from 0-10, 
+*   the green from 0-5, and the blue from 10 to 7, in
+*   ten steps.
+*   We'd want to count the 10 steps and increase or 
+*   decrease color values in evenly stepped increments.
+*   Imagine a + indicates raising a value by 1, and a -
+*   equals lowering it. Our 10 step fade would look like:
+* 
+*   1 2 3 4 5 6 7 8 9 10
+* R + + + + + + + + + +
+* G   +   +   +   +   +
+* B     -     -     -
+* 
+* The red rises from 0 to 10 in ten steps, the green from 
+* 0-5 in 5 steps, and the blue falls from 10 to 7 in three steps.
+* 
+* In the real program, the color percentages are converted to 
+* 0-255 values, and there are 1020 steps (255*4).
+* 
+* To figure out how big a step there should be between one up- or
+* down-tick of one of the LED values, we call calculateStep(), 
+* which calculates the absolute gap between the start and end values, 
+* and then divides that gap by 1020 to determine the size of the step  
+* between adjustments in the value.
+*/
+int calculateStep(int prevValue, int endValue) {
+    int step = endValue - prevValue; // What's the overall gap?
+    if (step) {                      // If its non-zero, 
+        step = 1020/step;            //   divide by 1020
+    }
+    
+    return step;
+}
+
+/* The next function is calculateVal. When the loop value, i,
+*  reaches the step size appropriate for one of the
+*  colors, it increases or decreases the value of that color by 1. 
+*  (R, G, and B are each calculated separately.)
+*/
+int calculateVal(int step, int val, int i) {
+    if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
+        if (step > 0) {              //   increment the value if step is positive...
+            val += 1;
+        }
+        else if (step < 0) {         //   ...or decrement it if step is negative
+            val -= 1;
+        }
+    }
+    
+    // Defensive driving: make sure val stays in the range 0-255
+    if (val > 255) {
+        val = 255;
+    }
+    else if (val < 0) {
+        val = 0;
+    }
+    
+    return val;
+}
+
