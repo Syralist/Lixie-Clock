27558707f442900c12544514701ae985a2c60ec3
Thomas Helmke <thomas.helmke@gmx.de>
2018-11-15 20:13:33 +0100

    OTA Name geändert

--- esp8266-clock/esp8266-clock.ino
+++ esp8266-clock/esp8266-clock.ino
@@ -1,7 +1,7 @@
 // Include Lixie Library
 #include "Lixie.h" 
 // setup number of pixels
-const bool SIX_DIGIT = false; // True if 6-digit clock with seconds
+const bool SIX_DIGIT = true; // True if 6-digit clock with seconds
 const int NUM_LIXIES = (SIX_DIGIT) ? 6 : 4; // 4 or 6 digits
 const int DISPLAY_INTERVAL = (SIX_DIGIT) ? 1000 : 60000; // update once a second or once a minute
 // setup pin number and create Lixie object
@@ -74,7 +74,7 @@ void setup()
   Serial.println(WiFi.localIP());
 
   // setup over the air programming
-  ArduinoOTA.setHostname("Wordclock12OTA");
+  ArduinoOTA.setHostname("LixieClockOTA");
   ArduinoOTA.onStart([]() {
           Serial.println("OTA Start");
           });
